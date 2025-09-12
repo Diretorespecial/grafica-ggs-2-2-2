09a9be0f6b99ef5a24d7a6cbfbbd35995de4daf7
Diretorespecial <diretorespecial@graficaggs.com.br>
2025-09-12 14:41:20 -0300

    Update main.js

--- GraficaGGS 2/js/main.js
+++ GraficaGGS 2/js/main.js
@@ -339,3 +339,8 @@ function checkimputs() {
 
   return allFieldsFilled;
 }
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then(reg => console.log('Service Worker registered', reg))
+    .catch(err => console.error('Service Worker **not** registered', err));
+}
