f2552d932451cce3acd78126b593c8c09cbd8b14
Diretorespecial <diretorespecial@graficaggs.com.br>
2025-09-17 15:41:42 -0300

    Update sw.js

--- GraficaGGS 2/sw.js
+++ GraficaGGS 2/sw.js
@@ -1,39 +1,22 @@
-const cacheName = 'grafica-ggs-cache-v1';
-const filesToCache = [
-  './',
-  'Formulario.html',
-  'index.html',
-  'css/main.css',
-  'css/style.css',
-  'js/main.js',
-  'js/firebase.js',
-  'images/logo.png',
-  'images/favicon.png',
-  'images/default_avatar.jpg',
-  'images/arrow.png',
-  'images/google.png',
-  'images/qrcode.png',
-  'assets/declaracao.png',
-  'assets/procuracao.png',
-  'assets/procuracao_a.png',
-  'assets/requisicao.png',
-  'assets/retinoides.png',
-  'assets/tipo_amarelo.png',
-  'assets/tipo_b.png',
-  'assets/tipo_b2.png'
-];
-
-self.addEventListener('install', e => {
+self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll(filesToCache);
+    caches.open('ggs-store').then((cache) => {
+      return cache.addAll([
+        '/GraficaGGS%202/',
+        '/GraficaGGS%202/index.html',
+        '/GraficaGGS%202/Formulario.html',
+        '/GraficaGGS%202/css/main.css',
+        '/GraficaGGS%202/css/style.css',
+        '/GraficaGGS%202/js/main.js',
+        '/GraficaGGS%202/images/logo.png'
+      ]);
     })
   );
 });
 
-self.addEventListener('fetch', e => {
+self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then(response => {
+    caches.match(e.request).then((response) => {
       return response || fetch(e.request);
     })
   );
